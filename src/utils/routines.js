41340a4708389a7671525c52aa65f1470ed67632
cheeric89 <felipealmfx@gmail.com>
2026-08-09 17:45:00 -0400

    feat: incluir calentamiento descanso y descarga en plantillas

--- src/utils/routines.js
+++ src/utils/routines.js
@@ -1,11 +1,24 @@
+import { normalizeExercisePrescription } from "./repRangeProgression";
+
+const buildTemplateSets = (exercise = {}) => {
+  const prescription = normalizeExercisePrescription(exercise);
+  const workingSetCount = prescription.deload
+    ? Math.max(1, prescription.sets - 1)
+    : prescription.sets;
+
+  return [
+    ...Array.from({ length: prescription.warmupSets }, () => ({ setType: "warmup" })),
+    ...Array.from({ length: workingSetCount }, () => ({ setType: "working" })),
+  ];
+};
+
 export const routineToTemplateCategories = (routine) =>
   (routine?.categories || []).map((category) => ({
     name: category.name,
     exercises: (category.exercises || []).map((exercise) => ({
       name: exercise.name,
-      repMin: exercise.repMin,
-      repMax: exercise.repMax,
-      sets: Array.from({ length: Math.max(1, Number(exercise.sets) || 1) }, () => ({})),
+      ...normalizeExercisePrescription(exercise),
+      sets: buildTemplateSets(exercise),
     })),
   }));
 
@@ -25,3 +38,30 @@ export const countRoutineSets = (routine) =>
       ),
     0
   );
+
+export const countRoutineWarmupSets = (routine) =>
+  (routine?.categories || []).reduce(
+    (total, category) =>
+      total +
+      (category.exercises || []).reduce(
+        (sum, exercise) => sum + Math.max(0, Number(exercise.warmupSets) || 0),
+        0
+      ),
+    0
+  );
+
+export const isRoutineDeload = (routine) =>
+  (routine?.categories || []).some((category) =>
+    (category.exercises || []).some((exercise) => exercise.deload === true)
+  );
+
+export const setRoutineDeload = (routine, active) => ({
+  ...routine,
+  categories: (routine?.categories || []).map((category) => ({
+    ...category,
+    exercises: (category.exercises || []).map((exercise) => ({
+      ...exercise,
+      deload: Boolean(active),
+    })),
+  })),
+});
