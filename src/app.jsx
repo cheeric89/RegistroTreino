1bdbc5df77d014f91d9abc2b7469e686fda7e5c3
cheeric89 <felipealmfx@gmail.com>
2026-08-09 18:51:53 -0400

    Permitir iniciar la rutina planificada desde el dashboard

--- src/app.jsx
+++ src/app.jsx
@@ -217,6 +217,7 @@ export default function App() {
             user={user}
             profile={profile}
             onStart={handleStart}
+            onStartRoutine={handleStartRoutine}
             onManageRoutines={handleManageRoutines}
             onOpenHistory={handleOpenHistory}
             onRepeatWorkout={handleRepeatWorkout}
