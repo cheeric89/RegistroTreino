cf54757deca356a30aef4ebc2ae8138892f90b43
cheeric89 <felipealmfx@gmail.com>
2026-09-01 09:14:28 -0400

    style(1.6.1): load polish overrides

--- src/index.jsx
+++ src/index.jsx
@@ -30,6 +30,7 @@ import "./styles/portfolio-daily-experience.css";
 import "./styles/portfolio-routine-launch.css";
 import "./styles/portfolio-workout-editor.css";
 import "./styles/portfolio-simplified-ux.css";
+import "./styles/portfolio-polish-1-6-1.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
